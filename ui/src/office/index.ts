9668e249c2cd9074c3403160ee05747795589d22
Jarvis237509 <novartism82@gmail.com>
2026-02-22 11:08:42 +1100

    Add Digital Office View - component index barrel exports

---
+++ ui/src/office/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Digital Office View - Component Exports
+ * 
+ * Barrel export file for all Office components.
+ */
+
+export { default as OfficeView } from './OfficeView';
+export { default as Workstation } from './Workstation';
+export { default as TeamZone } from './TeamZone';
+export { default as CeoOffice } from './CeoOffice';
+export { default as AgentAvatar } from './AgentAvatar';
+export { default as StatusIndicator } from './StatusIndicator';
+
+export * from './types';
+export * from './office-data';
+export * from './useOfficeState';
